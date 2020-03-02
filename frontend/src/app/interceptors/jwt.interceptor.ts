cc5790ad0b6a73eeee1af536edc8325b85e5d258
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2020-03-02 10:47:25 -0300

    Interceptador criado. Testarei depois

---
+++ frontend/src/app/interceptors/jwt.interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { StoreService } from '../services/store.service';
+import { API_CONFIG } from '../config/api-config';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+  constructor(private store: StoreService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let localUser = this.store.getLocalUser();
+    let n = API_CONFIG.baseUrl.length;
+    let requestToApi = request.url.substring(0, n) == API_CONFIG.baseUrl;
+
+    if (localUser && requestToApi) {
+      const authRequest = request.clone(
+        { headers: request.headers.set('Authorization', "Bearer " + localUser['access']) });
+      return next.handle(authRequest);
+    }
+
+    return next.handle(request);
+  }
+}
