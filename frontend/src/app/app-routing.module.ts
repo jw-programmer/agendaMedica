29eeeca20eb301bcaf421b0bea0ef887c3d4504d
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2020-03-03 23:19:11 -0300

    Ajustes desta versão feitos. Está versão está apresentavel para os avaliadores.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, 
+const routes: Routes = [{ path: '', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) }, 
 { path: 'consulta', loadChildren: () => import('./pages/consultas/consultas.module').then(m => m.ConsultasModule) }];
 
 @NgModule({
